14c811176fa66078ae7c3b3eeea175397819f625
senobi-social <bluesky.work17@gmail.com>
2020-05-23 13:15:04 +0900

    second commit

--- All.java
+++ All.java
@@ -23,17 +23,29 @@ public class All extends HttpServlet {
     Page.header(out);
 
     try {
-      InitialContext ic = new InitialContext();
-      DataSource ds = (DataSource)ic.lookup(
-      "jdbc:/comp/env/jdbc/book");
 
+      // コンストラクタを使ってInitialContextオブジェクトを取得
+      // JNDIを使うための窓口になる
+      // コネクションプールからコネクションを取得
+      InitialContext ic = new InitialContext();
+
+      // データソースの取得
+      // 因数にはJDNIのリソース名を指定する
+      // context.xmlのResource要素のname属性を参照
+      DataSource ds = (DataSource)ic.lookup("java:/comp/env/jdbc/book");
+
+      // データベースへの接続を確立
       Connection con = ds.getConnection();
 
+      // SQL文のセット
       PreparedStatement st = con.prepareStatement (
       "select * from temperature_members");
 
+      // SQL文の実行
       ResultSet rs = st.executeQuery();
 
+      // next()で１行ずつデータを取り出す
+      // next()は行があればtrue,なければfalseを返す
       while (rs.next()) {
         out.println(rs.getInt("id"));
         out.println(":");
@@ -43,7 +55,10 @@ public class All extends HttpServlet {
 
       }
 
+      // PreparedStatementを修了
       st.close();
+
+      // Connectionを終了
       con.close();
 
     } catch (Exception e ) {
